4e2a6de74bca6041a51517cb2e60a1b5a6415fd0
kokegbe <79014410+kokegbe@users.noreply.github.com>
2025-03-08 22:56:07 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ with st.expander('Data Visualization'):
   st.scatter_chart(data =df, x='bill_length_mm', y='body_mass_g', color='species')
 
 #Data Preparation
-with st.sidebar
+with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
   gender = st.selectbox('Gender',('male','female'))
