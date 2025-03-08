bffe509d871efb39f7cc4f3b2112295f231684e6
kokegbe <79014410+kokegbe@users.noreply.github.com>
2025-03-08 22:29:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,8 @@ st.title('Machine Learning App')
 
 st.write('This is a Machine Learning App')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
